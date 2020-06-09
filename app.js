9f7b6a8560b7e56cf008651d023411e60f13057d
Shrill Shrestha <shrill.shrestha@gmail.com>
2020-06-08 20:27:45 -0500

    Test: create dummy user

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-//import modules
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').config()
 
-//router modules
+//router route modules
 const shopRoute = require('./routes/shop');
 const adminRoute = require('./routes/admin');
 const errorRoute = require('./controllers/error');
@@ -12,32 +11,54 @@ const errorRoute = require('./controllers/error');
 //import database configuration
 const sequelize = require('./utils/datatbase');
 
-//create server app
+//import database modules
+const Product = require('./models/product');
+const User = require('./models/user');
+
 const app = express();
 
-//use middleware body-parser to parse incoming request body
 app.use(bodyParser.urlencoded({extended: false}));
 
-//set view engine and template location directory
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-//set static file location to public folder
 app.use(express.static(path.join(__dirname, "public")));
 
-//routing function
+app.use((req, res, next)=>{
+    User.findByPk(1)
+    .then((user)=>{
+        req.user = user;
+        next();
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+})
+
 app.use('/admin', adminRoute);
 app.use('/', shopRoute);
 
-//route to 404 error page
 app.get('*', errorRoute.get404);
+
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
 
 //sync to database
 sequelize.sync()
 .then(result =>{
+    return User.findByPk(1)
+})
+.then((user)=>{
+    if(!user){
+        return User.create({name: 'Shrill', email: 'test@gmail.com'});
+    }
+    return user;
+})
+.then((result)=>{
+    console.log(result);
     app.listen(3000, ()=>{
         console.log("Running in 3000!");
-    })
+    });
 })
 .catch(err=>{
     console.log(err)
